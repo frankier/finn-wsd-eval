ea5822171ac01267be1993d8614f27d715b3da0b
Frankie Robertson <frankie@robertson.name>
2018-09-13 09:38:24 +0300

    Fix working directory in ims support scripts

--- support/ims/test_ims.bash
+++ support/ims/test_ims.bash
@@ -6,4 +6,5 @@ fi
 modelDir=$1
 testFile=$2
 savePath=$3
+cd "$(dirname "$0")"
 gradle run -DmainClass=sg.edu.nus.comp.nlp.ims.implement.CTester --args="-prop jwnl-properties.xml -token 1 -pos 1 -split 1 -lemma 1 -r sg.edu.nus.comp.nlp.ims.io.CAllWordsResultWriter $testFile $modelDir $modelDir $savePath -f sg.edu.nus.comp.nlp.ims.feature.CAllWordsFeatureExtractorCombination"
