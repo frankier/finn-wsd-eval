2cc0ddd3cc15195b338eb8a440794b576b92f7d3
Frankie Robertson <frankie@robertson.name>
2018-10-06 18:36:06 +0300

    Add mk_test_as_train to make test_as_train corpus (where getting 100% is trivial)

---
+++ mk_test_as_train.py
@@ -0,0 +1,24 @@
+from os import makedirs, symlink, remove
+from os.path import abspath, lexists
+import click
+from stiff.eval import get_eval_paths
+
+
+@click.command()
+@click.argument("corpus_in", type=click.Path())
+@click.argument("corpus_out", type=click.Path())
+def main(corpus_in, corpus_out):
+    paths_in = get_eval_paths(corpus_in)
+    makedirs(corpus_out, exist_ok=True)
+    paths_out = get_eval_paths(corpus_out)
+    for partition in ["test", "train"]:
+        for tag in paths_in[partition]:
+            dest = paths_out[partition][tag]
+            if lexists(dest):
+                remove(dest)
+            src = abspath(paths_in["test"][tag])
+            symlink(src, dest)
+
+
+if __name__ == '__main__':
+    main()
