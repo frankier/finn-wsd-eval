0cc05c32eb2c2f03f08054783f964a86db8dd6de
Frankie Robertson <frankie@robertson.name>
2018-10-12 17:06:58 +0300

    Remove intermediate files when preparing ukb graph

--- support/ukb/prepare_wn30graph.sh
+++ support/ukb/prepare_wn30graph.sh
@@ -12,10 +12,14 @@ cd wn30
 # download and extract
 wget -q http://wordnetcode.princeton.edu/3.0/WordNet-3.0.tar.gz
 tar xzf WordNet-3.0.tar.gz
+rm WordNet-3.0.tar.gz
 wget -q http://wordnetcode.princeton.edu/glosstag-files/WordNet-3.0-glosstag.tar.bz2
 tar xjf WordNet-3.0-glosstag.tar.bz2 
+rm WordNet-3.0-glosstag.tar.bz2 
 # create ukb relation files from source (including gloss relations)
 perl ${script_dir}/wnet2graph.pl WordNet-3.0/dict/* > wn30_rel.txt
 perl ${script_dir}/wnetgloss2graph.pl WordNet-3.0/dict/index.sense  WordNet-3.0/glosstag/merged > wn30_gloss_rel.txt
+rm -rf WordNet-3.0
 # compile graph
 cat wn30_rel.txt wn30_gloss_rel.txt | ${abspath_to_ukb}/compile_kb -o wn30g.bin --note "wn30_rel.txt wn30_gloss_rel.txt" -
+rm wn30_rel.txt wn30_gloss_rel.txt
