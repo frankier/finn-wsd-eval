c9885fad9b413d38a6f67afa79ed94ee62234e06
Frankie Robertson <frankie@robertson.name>
2018-10-16 09:33:04 +0300

    Allow unknown guesses in write_lemma

--- utils.py
+++ utils.py
@@ -15,13 +15,18 @@ def lemmas_from_instance(wn, instance):
 
 def write_lemma(keyout, inst_id, lemma):
     fi2en, en2fi = get_en_fi_maps()
-    chosen_synset_fi_id = ss2pre(lemma.synset())
-    if chosen_synset_fi_id not in fi2en:
-        sys.stderr.write(
-            "No fi2en mapping found for {} ({})\n".format(chosen_synset_fi_id, lemma)
-        )
-        return
-    keyout.write("{} {}\n".format(inst_id, pre_id_to_post(fi2en[chosen_synset_fi_id])))
+    if lemma is None:
+        guess = "U"
+    else:
+        chosen_synset_fi_id = ss2pre(lemma.synset())
+        if chosen_synset_fi_id not in fi2en:
+            sys.stderr.write(
+                "No fi2en mapping found for {} ({})\n".format(chosen_synset_fi_id, lemma)
+            )
+            guess = "U"
+        else:
+            guess = pre_id_to_post(fi2en[chosen_synset_fi_id])
+    keyout.write("{} {}\n".format(inst_id, guess))
 
 
 def unigram(inf, keyout, wn):
