9a5f28a7d2a15088318ae18fe6afd4f99eeb291c
Frankie Robertson <frankie@robertson.name>
2019-07-22 11:44:19 +0300

    Use

--- install_powerai.sh
+++ install_powerai.sh
@@ -1,9 +1,29 @@
 set -euxo pipefail
 
+cpip() {
+    ERROR_MSG="Not in a conda environment."
+    ERROR_MSG="$ERROR_MSG\nUse \`source activate ENV\`"
+    ERROR_MSG="$ERROR_MSG to enter a conda environment."
+
+    [ -z "$CONDA_DEFAULT_ENV" ] && echo "$ERROR_MSG" && return 1
+
+    ERROR_MSG='Pip not installed in current conda environment.'
+    ERROR_MSG="$ERROR_MSG\nUse \`conda install pip\`"
+    ERROR_MSG="$ERROR_MSG to install pip in current conda environment."
+
+    [ -e "$CONDA_PREFIX/bin/pip" ] || (echo "$ERROR_MSG" && return 2)
+
+    PIP="$CONDA_PREFIX/bin/pip"
+    "$PIP" "$@"
+}
+
+# Add channels
+conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+
 ## Install
-conda install powerai
-conda install pip
-conda run pip install poetry
+conda install -y powerai
+conda install -y pip
+conda run cpip install poetry
 conda run poetry export --without-hashes -f requirements.txt
 # Obviously not compatible
 sed -i '/pypiwin32/d' requirements.txt
@@ -11,7 +31,7 @@ sed -i '/pywin32/d' requirements.txt
 # Already installed
 sed -i '/sklearn/d' requirements.txt
 sed -i '/torch/d' requirements.txt
-conda run pip install --pre -r requirements.txt
+conda run cpip install --pre -r requirements.txt
 
 ## Init
 conda run python -c "from nltk import download as d; d('wordnet'); d('omw'); d('punkt')"
