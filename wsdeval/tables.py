fb8a4dd329611ecbe00b9321fb5ea450884746e8
Frankie Robertson <frankie@robertson.name>
2019-11-15 21:37:07 +0200

    Update tables

--- wsdeval/tables.py
+++ wsdeval/tables.py
@@ -129,20 +129,20 @@ LESK_PP_SQUARE_SPEC = SqTableSpec(
     DimGroups(
         [
             DEV_CORPORA,
-            EXPAND_GROUP_DISP,
             LookupGroupDisplay(
                 CatValGroup("mean", ["pre_sif_mean", "normalized_mean"]),
                 {"normalized_mean": "AWE", "pre_sif_mean": "pre-SIF"},
+            ),
+            LookupGroupDisplay(
+                CatValGroup("score_by", ["both", "defn", "lemma"]),
+                {"both": "Both", "defn": "Defn", "lemma": "Lemma"},
             ),
         ],
         div_idx=0,
     ),
     DimGroups(
         [
-            LookupGroupDisplay(
-                CatValGroup("score_by", ["both", "defn", "lemma"]),
-                {"both": "Both", "defn": "Defn", "lemma": "Lemma"},
-            ),
+            EXPAND_GROUP_DISP,
             LookupGroupDisplay(
                 tf_group("exclude_cand"), {False: "Incl. cand", True: "Excl. cand"}
             ),
